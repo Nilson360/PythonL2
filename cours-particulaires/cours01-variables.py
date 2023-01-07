8530b53779077496f4c7614ee3b6fbc5dfcf29ed
Nilson360 <84663139+Nilson360@users.noreply.github.com>
2023-01-07 21:23:57 +0100

    reaname file

--- cours-particulaires/cours01-variables.py
+++ cours-particulaires/cours01-variables.py
@@ -4,3 +4,14 @@ z = float(3)
 print(type(x)) #voir le type de la variable
 print(type(y))
 print(type(z))
+
+
+#Many values to multiple variables
+
+a,b,c = "Orange", "Banana", "Cherry"
+print(a)
+print(b)
+print(c)
+
+#One value for multiple variables
+
