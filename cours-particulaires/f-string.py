843567d1d07687eedd6b8943e396ea02bbb1e178
Nilson360 <84663139+Nilson360@users.noreply.github.com>
2023-01-08 16:59:48 +0100

    update file

--- cours-particulaires/f-string.py
+++ cours-particulaires/f-string.py
@@ -1,8 +1,10 @@
-#exemple 1
+#exemple 1 
 x = 32
 nom ="John"
 print(f"{nom} a {x} ans")
 
-#exemple 2
+#exemple 2 - utiliser une variable à plusieurs reprises
 var ="to"
 print(f"{var} et {var} font {var}{var}")
+
+#exemple 3 - utilisation des valeurs numériques et des 
