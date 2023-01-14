fd7a6a781d167080cd5a0b6deb99ed21f4712a9a
Nilson360 <84663139+Nilson360@users.noreply.github.com>
2023-01-14 12:43:39 +0100

    update files

--- cours-particulaires/f-string.py
+++ cours-particulaires/f-string.py
@@ -12,3 +12,9 @@ print("----------Gerenado --------------")
 print(1j*(1+2j)) #nombre complexe
 print(type(1j*(1+2j))) 
 print(1.1+1/3)
+
+
+#Exemple de l'utilisation des f-string
+print("---------------- GC exemple --------------------------")
+prop_GC  = (4500 + 2575)/14800
+print(f"La proportion de GC est {prop_GC:.4f}")
