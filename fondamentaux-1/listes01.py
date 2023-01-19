212aa32a63919adde5e4784354321976189b540b
Nilson360 <84663139+Nilson360@users.noreply.github.com>
2023-01-19 23:08:09 +0100

    update file

--- fondamentaux-1/listes01.py
+++ fondamentaux-1/listes01.py
@@ -53,3 +53,45 @@ print("---------------- TRANCHES AVEC PAS----------------")
 X = [0,1,2,3,4,5,6,7,8,9]
 print(animaux[0:3:2])
 print(X[1:6:3])
+
+# FONCTION len()
+# La fonction len() nous permet de connaître la taille d'une liste
+print("La liste a ", len(animaux) ," éléments")
+print("La liste a ", len(X) ," éléments")
+
+# FONCTION range() et list()
+# la méthode range() génére des nombres intiers compris dans un intervalle.
+# la méthode list() nous permet de créer une liste dinamiquement
+# nous pouvons combiner ces deux fonctions :
+
+list01 = list(range(10)) #la combinaison de ceux des méthodes a généré une liste contenant tous nombres entiers de 0 inclus à 10 exclu
+x = list(range(0,5))
+b = list(range(15,20))
+print(list01) 
+print(x)
+print(b)
+
+# nous pouvons aussi définir le pas dans la fonction range
+#range(début,fin,pas)
+number = list(range(0,1000,200))
+number01 = list(range(10,0,-1))
+print(number)
+print(number01)
+
+# LISTE DE LISTES
+# Python nous permet de créer une liste qu'elle même garde d'autres listes
+
+# EXEMPLE:
+enclos1 = ["girafe", 4]
+enclos2 = ["tigre", 2]
+enclos3 = ["singe", 5]
+zoo = [enclos1,enclos2,enclos3]
+print(zoo)
+
+# Dans cet exemple, chaque sous-liste contient une catégorie d'animal et le nombre d'animaux
+# Pour accéder à un élément de la liste, on utilise l'indiçage habituel
+
+print("élément de la liste", zoo[1])
+
+# Pour accéder à un élément de la sous-liste, on utilise un double indiçage:
+print("sous liste ", zoo[1][0])
