1471442e87a1fd7427b5eeb16e7399ef82d61c54
Nilson360 <84663139+Nilson360@users.noreply.github.com>
2023-01-19 20:56:39 +0100

    add listes

---
+++ fondamentaux-1/listes01.py
@@ -0,0 +1,14 @@
+# Étude des listes avec python - Calcul approfondie 2023
+# 
+# Une liste est une structure de données qui contient une série de valeurs.
+# Dans une liste nous pouvons avoir des données de differents types ou du même type (int, float string)
+
+# EXEMPLE DE LISTES 
+animaux = ["girafe", "tigre", "singe","souris"]
+tailles = [5, 2.5, 1.75, 0.15] 
+mixte = ["girafe", 5, "souris", 0.15]
+
+# La première liste est composée que sur des données du type String
+# Le seconde tous les données sont du type Number(int et float)
+# Le troisième est un mix des deux , int, string et float.
+
