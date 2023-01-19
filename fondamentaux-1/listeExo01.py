cebb4d93f3e124c38d7c003c5fd0c726f1d01be2
Nilson360 <84663139+Nilson360@users.noreply.github.com>
2023-01-19 23:18:06 +0100

    add exo

---
+++ fondamentaux-1/listeExo01.py
@@ -0,0 +1,10 @@
+# Jours de la semaine
+# Constituez une liste semaine contenant les 7 jours de la semaine.
+# 1. À partir de cette liste, comment récupérez-vous seulement les 5 premiers jours de la semaine d’une part, et ceux du
+# week-end d’autre part ? Utilisez pour cela l’indiçage.
+# 2. Cherchez un autre moyen pour arriver au même résultat (en utilisant un autre indiçage).
+# 3. Trouvez deux manières pour accéder au dernier jour de la semaine.
+# 4. Inversez les jours de la semaine en une commande.
+
+# 1.
+jSemaines = ["Lundi","Mardi","Mercredi","Jeudi","Vendredi","Samedi","Dimanche"]
