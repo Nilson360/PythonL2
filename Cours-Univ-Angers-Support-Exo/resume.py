8b18307dbc9269450254e843b72ab346a711592b
Nilson360 <84663139+Nilson360@users.noreply.github.com>
2023-02-27 09:27:24 +0100

    add files

--- Cours-Univ-Angers-Support-Exo/resume.py
+++ Cours-Univ-Angers-Support-Exo/resume.py
@@ -3,3 +3,11 @@ print(type("abc"))
 
 from math import sqrt # autre forme d'importer une fonction en ciblant l'élément volue
 print(sqrt(4))
+(-1.1).is_integer()
+print(id("adb")) #trouver l'adresse d'une variable
+
+print(type(1j+(1+2j)))
+from math import cos,tan,sin
+print(f"Le cossenus de l'angle 14 est {cos(14)}")
+print(f"Le sinus de l'angle 14 est {sin(14)}")
+print(f"La tengant de l'angle 14 est {tan(14)}")
